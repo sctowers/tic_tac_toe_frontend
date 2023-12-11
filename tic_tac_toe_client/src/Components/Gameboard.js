fbdc988866d002fc3a5ef9c745a2248c5e1c5010
Sarah Ogunko <sarahogunko@hotmail.com>
2023-12-11 14:22:57 +0000

    fixed component and container set up

--- tic_tac_toe_client/src/Components/Gameboard.js
+++ tic_tac_toe_client/src/Components/Gameboard.js
@@ -1,13 +1,10 @@
 import CellButton from "./CellButton";
-import Navigation from "./Navigation";
 import GameStatus from "./GameStatus";
-import StartGameForm from "./StartGameFrom";
 const Gameboard = () => {
 
     return ( 
         <>
-        <Navigation />
-        < StartGameForm />
+      
         <CellButton />
         <GameStatus />
         </>
--- tic_tac_toe_client/src/Container/HomePage.js
+++ tic_tac_toe_client/src/Container/HomePage.js
@@ -1,10 +1,13 @@
 import Gameboard from "../Components/Gameboard";
 import StartGameForm from "../Components/StartGameFrom";
+import Navigation from "../Components/Navigation";
+
 const HomePage = () => {
     return ( 
 
         <>
         <h1>TicTacToe🕹️</h1>
+        <Navigation />
         <Gameboard />
         <StartGameForm />
         </>
