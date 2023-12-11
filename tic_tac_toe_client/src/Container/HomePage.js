cf602a6fe9245ce55861ff2fb53f116c893f2e78
Emily Reid <emily.reid31@gmail.com>
2023-12-11 15:23:22 +0000

    adding in progress bug for started get fetch request

--- tic_tac_toe_client/src/Container/HomePage.js
+++ tic_tac_toe_client/src/Container/HomePage.js
@@ -1,18 +1,35 @@
+import { useState } from "react";
 import Gameboard from "../Components/Gameboard";
 import StartGameForm from "../Components/StartGameFrom";
 import Navigation from "../Components/Navigation";
 
 const HomePage = () => {
-    return ( 
+    const [game, setGame] = useState([]);
 
+    const startGame = async (difficulty, playerId) => {
+        const response = await fetch("https://localhost:8080/games", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({difficulty, playerId}),
+
+
+        })
+        const newGame = await response.json();
+        setGame(newGame);
+        
+        console.log(newGame);
+    }
+    startGame("EASY", 1);
+    return (
         <>
-        <h1>TicTacToe🕹️</h1>
-        <Navigation />
-        <Gameboard />
-        <StartGameForm />
+            <h1>TicTacToe🕹️</h1>
+            <Navigation />
+            <Gameboard />
+            <StartGameForm />
         </>
 
-     );
+    );
+
 }
- 
+
 export default HomePage;
