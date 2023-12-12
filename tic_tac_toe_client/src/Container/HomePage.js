9567fcc8368edd4a6b6f64a3a3d9401444b5d129
Thomas McLaughlin <tmclaughlin5.1111@gmail.com>
2023-12-12 15:39:50 +0000

    game logic working up to 1 move

--- tic_tac_toe_client/src/Container/HomePage.js
+++ tic_tac_toe_client/src/Container/HomePage.js
@@ -5,52 +5,70 @@ import StartGameForm from "../Components/StartGameForm";
 import Navigation from "../Components/Navigation";
 
 const HomePage = () => {
-    const [game, setGame] = useState({ board: [] });
-    
-    const startGame = async (difficulty, playerId) => {
-        const response = await fetch("http://localhost:8080/games", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ difficulty, playerId }),
+  const [game, setGame] = useState({ board: [] });
+//   const [move, setMove] = useState({ board: [] });
 
-        })
-        const newGame = await response.json();
-        const gameStateResponse = await fetch(`http://localhost:8080/games/game-state/${newGame.id}`)
-        const gameWithBoard = await gameStateResponse.json();
-
-        setGame(gameWithBoard);
-        
-    }
-
-
-    const gameRoutes = createBrowserRouter([
-        {
-            path: "/",
-            element: <Navigation/>,
-            children: [
-                {
-                    path: "/game",
-                    element: (
-                        <StartGameForm startGame={startGame}/>
-                    ),
-                },
-            {
-                    path:"/gameboard",
-                    element:(<Gameboard game={game}/>),
-            }
-        ]
-        }
-]);
-
-    return (
-        <>
-            <h1>TicTacToe🕹️</h1>
-            <RouterProvider router={gameRoutes}/>
-        
-        </>
-
+  const startGame = async (difficulty, playerId) => {
+    const response = await fetch("http://localhost:8080/games", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ difficulty, playerId }),
+    });
+    const newGame = await response.json();
+    const gameStateResponse = await fetch(
+      `http://localhost:8080/games/game-state/${newGame.id}`
     );
+    const gameWithBoard = await gameStateResponse.json();
 
-}
+    setGame(gameWithBoard);
+  };
+
+  const onCellClick = async (rowIndex, colIndex, cell) => {
+    if (cell === "EMPTY") {
+      let position = rowIndex * 3 + colIndex + 1;
+
+      try {
+        const response = await fetch(
+        `http://localhost:8080/games/${game.id}`,
+        {
+          method: "PATCH",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ position: +position }),
+        }
+      );
+      const newMove = await response.json();
+      setGame(newMove);
+        
+      } catch (error) {
+        console.log(error)
+      }
+      
+    }
+  };
+
+  const gameRoutes = createBrowserRouter([
+    {
+      path: "/",
+      element: <Navigation />,
+      children: [
+        {
+          path: "/game",
+          element: <StartGameForm startGame={startGame} />,
+        },
+        {
+          path: "/gameboard",
+          element: <Gameboard onCellClick={onCellClick} game={game} />,
+        },
+      ],
+    },
+  ]);
+
+  return (
+    <>
+      <h1>TicTacToe🕹️</h1>
+      <RouterProvider router={gameRoutes} />
+    </>
+  );
+};
 
 export default HomePage;
