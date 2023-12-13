91c3c8ffa93aba142c4037adb9a41ebada5361cc
Thomas McLaughlin <tmclaughlin5.1111@gmail.com>
2023-12-13 17:41:05 +0000

    more CSS

--- tic_tac_toe_client/src/Components/GameSound.js
+++ tic_tac_toe_client/src/Components/GameSound.js
@@ -1,47 +1,46 @@
-import React, { useState } from 'react';
-import Sound from 'react-sound';
-import Music from '../Assets/music.mp3'
-
+import React, { useState } from "react";
+import Sound from "react-sound";
+import Music from "../Assets/music.mp3";
 
 // Function Example from:https://dev.to/daveguz97/adding-sound-to-a-react-project-51m3 //
 const GameSound = (
-    handleSongLoading,
-    handleSongPlaying,
-    handleSongFinishedPlaying
-    ) => {
+  handleSongLoading,
+  handleSongPlaying,
+  handleSongFinishedPlaying
+) => {
+  // const[volume, setVolume] = useState(0);
+  // const[position, setPosition] = useState(1000);
+  let volume = 50;
 
-        const[volume, setVolume] = useState(0);
-        // const[position, setPosition] = useState(1000);
+  function mute() {
+    if (volume === 0) {
+      volume = 50;
+    } else {
+      volume = 0;
+    }
 
-        function mute(){
-            if (volume === 0) {
-                setVolume(50);
-            }
-            else {
-                setVolume(0);
-            }
+    let button = document.getElementById("muteButton");
+    button.innerText === "🔇"
+      ? (button.innerText = "🔈")
+      : (button.innerText = "🔇");
+  }
 
-            let button = document.getElementById("muteButton")
-    button.innerText === '🔇' ?  button.innerText = '🔈' : button.innerText = '🔇'
-        }
-    
-        
-    return (
-        <div>
-
-            {/* <button onClick={() => setIsPlaying(!isPlaying)}>{!isPlaying ? 'Play' : 'Stop'}   {/* //think like IF ElSE Tom */}
-            {/* </button> */} 
-           <button id='muteButton' onClick={mute}>🔇</button>  
-            <Sound
-                url={Music}
-                // playStatus={Sound.status.PLAYING}
-                playFromPosition={400} 
-                // loop={true}
-                volume={volume} 
-            />
-        </div>
-
-    );
-}
+  return (
+    <div>
+      {/* <button onClick={() => setIsPlaying(!isPlaying)}>{!isPlaying ? 'Play' : 'Stop'}   {/* //think like IF ElSE Tom */}
+      {/* </button> */}
+      <button id="muteButton" onClick={mute}>
+        🔇
+      </button>
+      <Sound
+        url={Music}
+        playStatus={Sound.status.PLAYING}
+        playFromPosition={300}
+        loop={true}
+        volume={volume}
+      />
+    </div>
+  );
+};
 
 export default GameSound;
