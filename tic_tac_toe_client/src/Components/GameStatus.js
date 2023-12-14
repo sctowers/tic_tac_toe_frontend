de8cf012f875756ac8861a0cf6c2d176b5d08b08
Suzi Towers <suzitowers@gmail.com>
2023-12-14 14:54:36 +0000

    change colour of game result depending on game result

--- tic_tac_toe_client/src/Components/GameStatus.js
+++ tic_tac_toe_client/src/Components/GameStatus.js
@@ -3,22 +3,31 @@ import { useWindowSize } from 'react-use';
 import { useState, useEffect } from 'react';
 const GameStatus = ({game}) => {
     const [confetti, setConfetti] = useState(false);
+
     const{width , height}= useWindowSize();
     useEffect(() => {
-      
         if (game.result === "WIN") {
           setConfetti(true);
-      
-       
         } else setConfetti(false);
     }, [game.result]);
-      
+
+    const getResultColour = () => {
+      if (game.result === 'WIN') {
+        return {color: 'green'}
+      } else if (game.result === 'LOSS') {
+        return {color: 'red'}
+      } else {
+        return {color: '#385170'}
+      }
+    }
+
     return ( 
         <div>
-        <p id="gameResult">Game Result: {game.result}</p>
+        <p id="gameResult" style={getResultColour()}>
+          Game Result: {game.result}</p>
         {confetti && <Confetti width={width} height={height} /> }
         </div>
-     );
+    );
 }
- 
+
 export default GameStatus;
