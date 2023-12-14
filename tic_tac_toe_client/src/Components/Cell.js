2b8f51d39d35895284f5d52c23090a5554f9be38
Thomas McLaughlin <tmclaughlin5.1111@gmail.com>
2023-12-14 15:22:45 +0000

    sound working but mute button restarts playhead to 0....don't use React-sound....research libraries before choosing

--- tic_tac_toe_client/src/Components/Cell.js
+++ tic_tac_toe_client/src/Components/Cell.js
@@ -1,6 +1,12 @@
-const Cell = ({ value, onClick, }) => {
+const Cell = ({ value, onClick, player }) => {
 
     let valueName = Array.from(value)[0];
+
+    // Emmanuel Face
+    // if (player === "Emmanuel") {
+    //     valueName = "A"
+    // }
+
     return ( 
         <div className={valueName} onClick={onClick}>
             {value}
