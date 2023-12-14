b47900e2102dcf939b19e4eeb58546fff50df281
Sarah Ogunko <sarahogunko@hotmail.com>
2023-12-14 12:19:59 +0000

    added confetti to win game status

--- tic_tac_toe_client/src/Components/GameStatus.js
+++ tic_tac_toe_client/src/Components/GameStatus.js
@@ -1,6 +1,22 @@
+import Confetti from 'react-confetti';
+import { useState, useEffect } from 'react';
 const GameStatus = ({game}) => {
+    const [confetti, setConfetti] = useState(false);
+
+    useEffect(() => {
+      
+        if (game.result === "WIN") {
+          setConfetti(true);
+      
+       
+        } else setConfetti(false);
+    }, [game.result]);
+      
     return ( 
+        <div>
         <p id="gameResult">Game Result: {game.result}</p>
+        {confetti && <Confetti /> }
+        </div>
      );
 }
  
